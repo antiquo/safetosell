078391dec7b44afad80bca573b20df502a6cb54e
antiquo <nastassia.gorbunova@gmail.com>
2017-07-04 21:55:29 +0300

    CutUser class is updated

--- src/main/java/by/antiquo/safetosell/entity/CutUser.java
+++ src/main/java/by/antiquo/safetosell/entity/CutUser.java
@@ -9,6 +9,8 @@ public class CutUser extends Entity {
     private String userLogin;
     private String userPic;
 
+    public CutUser(){}
+
     public CutUser(int userId, UserRole userRole, String userLogin, String userPic){
         this.userId = userId;
         this.userRole = userRole;
