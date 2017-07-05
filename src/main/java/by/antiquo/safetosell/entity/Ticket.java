f52b4a76c698bc487e6b83dd8813361bfbb27bd8
antiquo <nastassia.gorbunova@gmail.com>
2017-07-05 21:55:56 +0300

    Ticket class was updated

--- src/main/java/by/antiquo/safetosell/entity/Ticket.java
+++ src/main/java/by/antiquo/safetosell/entity/Ticket.java
@@ -14,4 +14,70 @@ public class Ticket extends Offer {
         this.ticketDesc = ticketDesc;
         this.ticketPic = ticketPic;
     }
+
+    public int getTicketId() {
+        return ticketId;
+    }
+
+    public void setTicketId(int ticketId) {
+        this.ticketId = ticketId;
+    }
+
+    public String getTicketName() {
+        return ticketName;
+    }
+
+    public void setTicketName(String ticketName) {
+        this.ticketName = ticketName;
+    }
+
+    public String getTicketDesc() {
+        return ticketDesc;
+    }
+
+    public void setTicketDesc(String ticketDesc) {
+        this.ticketDesc = ticketDesc;
+    }
+
+    public String getTicketPic() {
+        return ticketPic;
+    }
+
+    public void setTicketPic(String ticketPic) {
+        this.ticketPic = ticketPic;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+
+        Ticket ticket = (Ticket) o;
+
+        if (ticketId != ticket.ticketId) return false;
+        if (!ticketName.equals(ticket.ticketName)) return false;
+        if (!ticketDesc.equals(ticket.ticketDesc)) return false;
+        return ticketPic.equals(ticket.ticketPic);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + ticketId;
+        result = 31 * result + ticketName.hashCode();
+        result = 31 * result + ticketDesc.hashCode();
+        result = 31 * result + ticketPic.hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Ticket{ " +
+                "ticketId=" + ticketId +
+                ", ticketName='" + ticketName +
+                ", ticketDesc='" + ticketDesc +
+                ", ticketPic='" + ticketPic +
+                " }";
+    }
 }
